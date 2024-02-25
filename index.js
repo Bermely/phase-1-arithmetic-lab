c9b5140c8dd2e5fe9f42c71c30fed3047251b39e
Bermely Ferreras <bfbferreras@gmail.com>
2024-02-24 19:43:33 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2; 
+let multiply = num1 * num2; 
+console.log(multiply);
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+let num3 = 34;
+let num4 = 6; 
+let mod = num3 % num4; 
+console.log(mod);
+const numbers = [10,5,20,15];
+const max = Math.max(10,5,20,15);
+console.log(max);
